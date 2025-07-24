6e9bffa5954780098bc1fde0c759443cba78c59d
youssef <ym658089@gmail.com>
2025-07-25 02:21:58 +0300

    edit DB

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const db_connection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect('mongodb+srv://youssef:222001@cluster0.4jkqi.mongodb.net/E-comm-app');
     console.log("Database connected successfully");
   } catch (error) {
     console.log("Error in database connection: ", error.message);
